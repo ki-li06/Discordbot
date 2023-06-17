aafe8836245054a1b844d167dfc7f1a1022a5d28
Kilian <kilian@lindmeyr.com>
2023-06-17 15:55:53 +0200

    delete my token

--- src/main/java/JDA/core/Config.java
+++ src/main/java/JDA/core/Config.java
@@ -1,6 +1,6 @@
 package JDA.core;
 
 public class Config {
-    public static String token = "OTEwMTc2Nzc2MDMzMDM4MzY3.YZPCLQ.JXPWOhIze1ROxda8kRedHI8Xirk";
+    public static String token = "";
     public static Long AdminID = 702821727004000307L;
 }
